1b558bd16804a988df41374f1ffd8d50756718d6
Jon Bowen <jonbowen@uvic.ca>
2017-07-22 21:47:42 -0700

    change dummy code

--- dummyfile.js
+++ dummyfile.js
@@ -1,1 +1,17 @@
 // haha lol this is a comment
+
+var redpill = "red";
+
+
+redpill = redpill + redpill;
+
+var bluepill = "blue";
+
+var result = redpill + bluepill + redpill;
+
+console.log("redpill = "+ redpill);
+console.log("bluepill = "+ bluepill);
+console.log("result = "+ result);
+
+
+
